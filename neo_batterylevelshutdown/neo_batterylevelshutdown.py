953e2561ff22ee51e5da147ec1f002f68d401b69
GeoDirk <geodirk@gmail.com>
2017-12-08 11:02:55 +0300

    updated code and validated on HAT
    Code changes required to ensure that a HAT-less NEO wouldn't auto shutdown if the code ran on it.  Changed a lot of the file write code where I discovered that unless you closed out the file, the OS wouldn't recognize the file changes.

--- neo_batterylevelshutdown/neo_batterylevelshutdown.py
+++ neo_batterylevelshutdown/neo_batterylevelshutdown.py
@@ -25,7 +25,7 @@ GPIO_EXPORT_FILE = "/sys/class/gpio/export"
 PIN_HIGH = "1"
 PIN_LOW = "0"
 
-def setup_gpio_pin(pinNum, direction):
+def setup_gpio_pin(pinNum, direction, resistor):
     """Setup the GPIO Pin for operation in the system OS"""
     if not os.path.isfile(GPIO_EXPORT_FILE):
         logging.warn("Unable to find GPIO export file: %s "
@@ -37,11 +37,18 @@ def setup_gpio_pin(pinNum, direction):
     if not os.path.isfile(pinPath):
         try:
             # Export pin for access
-            with open(GPIO_EXPORT_FILE, "w") as f:
-                f.write(pinNum)
+            # with open(GPIO_EXPORT_FILE, "w") as f:
+            #     f.write(str(pinNum))
+            os.system("echo {} > /sys/class/gpio/export".format(pinNum))
+            logging.info("GPIO pin %s Export Complete", pinNum)
             # Configure the pin direction
-            with open(os.path.join(pinPath, "direction")) as f:
+            with open(os.path.join(pinPath, "direction"), 'w') as f:
                 f.write(direction)
+            logging.info("GPIO pin %s Direction Complete", pinNum)
+            # Configure the pull up (1) / pull down resistor (0)
+            with open(os.path.join(pinPath, "active_low"), 'w') as f:
+                f.write(str(resistor))
+            logging.info("GPIO pin %s active_low Complete", pinNum)
         except OSError:
             logging.warn("Error setting up GPIO pin %s", pinNum)
             return False
@@ -52,15 +59,13 @@ def setup_gpio_pin(pinNum, direction):
 def blink_LEDxTimes(pinNum, times):
     """Blink the LED a certain number of times"""
     try:
-        with open("/sys/class/gpio/gpio{}/value".format(pinNum), "w") as pin:
-            for _ in range(0, times):
-                pin.write(PIN_LOW)
-                time.sleep(LED_FLASH_DELAY_SEC)
+        for _ in range(0, times):
+            with open("/sys/class/gpio/gpio{}/value".format(pinNum), "w") as pin:
                 pin.write(PIN_HIGH)
-                time.sleep(LED_FLASH_DELAY_SEC)
-
-            # make sure that we turn the LED off
-            pin.write(PIN_HIGH)
+            time.sleep(LED_FLASH_DELAY_SEC)
+            with open("/sys/class/gpio/gpio{}/value".format(pinNum), "w") as pin:
+                pin.write(PIN_LOW)
+            time.sleep(LED_FLASH_DELAY_SEC)
     except OSError:
         logging.warn("Error writing to pin {}".format(pinNum))
         return False
@@ -69,39 +74,36 @@ def blink_LEDxTimes(pinNum, times):
 
 def readPin(pinNum):
     """Read the value from some input pin"""
+    logging.info("Reading pin {}".format(pinNum))
     try:
-        with open("/sys/class/gpio/gpio{}/value".format(pinNum)) as pin:
-            return pin.read(1) == 1
+        with open("/sys/class/gpio/gpio{}/value".format(pinNum), 'r') as pin:
+            return str(pin.read(1)) == "1"
     except OSError:
         logging.warn("Error reading from pin {}".format(pinNum))
-
     return -1
 
-
 def entryPoint():
+#if __name__ == "__main__":
+    #logging.basicConfig(filename='/root/output.log', filemode='w', level=logging.DEBUG)
     logging.info("Intializing Pins")
-    logging.debug("Pin: LED")
-    setup_gpio_pin(PIN_LED, "out")
-    logging.debug("Pin: PG6 3.0V")
-    setup_gpio_pin(PIN_VOLT_3_0, "in")
-    logging.debug("Pin: PG7 3.2V")
-    setup_gpio_pin(PIN_VOLT_3_2, "in")
-    logging.debug("Pin: PG8 3.4V")
-    setup_gpio_pin(PIN_VOLT_3_4, "in")
-    logging.debug("Pin: PG9 3.6V")
-    setup_gpio_pin(PIN_VOLT_3_6, "in")
+    setup_gpio_pin(PIN_LED, "out", 0)
+    setup_gpio_pin(PIN_VOLT_3_0, "in", 1) # set the pulldown resistor
+    setup_gpio_pin(PIN_VOLT_3_2, "in", 0)
+    setup_gpio_pin(PIN_VOLT_3_4, "in", 0)
+    setup_gpio_pin(PIN_VOLT_3_6, "in", 0)
 
-    logging.info("Starting Monitoring")
     iIteration = 0
     threads = []
     bContinue = True
+    logging.info("Starting Monitoring")
     while bContinue:
         # check if voltage is above 3.6V
         PIN_VOLT_ = readPin(PIN_VOLT_3_6)
+        logging.info("Value PIN_VOLT_3_6: {}".format(PIN_VOLT_))
         if PIN_VOLT_:
             try:
-                with open("/sys/class/gpio/gpio{}/value".format(PIN_LED)) as pin:
-                    pin.write(PIN_HIGH)
+                with open("/sys/class/gpio/gpio{}/value".format(PIN_LED), "w") as pin:
+                    pin.write(PIN_LOW)
             except OSError:
                 logging.warn("Error writing to pin {}".format(PIN_LED))
             time.sleep(9)
@@ -113,7 +115,7 @@ def entryPoint():
                                      args=(PIN_LED, 1,))
                 threads.append(t)
                 t.start()
-                time.sleep(9)
+                time.sleep(6)
             else:
                 # check if voltage is above 3.2V
                 PIN_VOLT_ = readPin(PIN_VOLT_3_2)
@@ -148,6 +150,5 @@ def entryPoint():
                             t.start()
 
         time.sleep(1)
-
     logging.info("Exiting for Shutdown\n")
     os.system("shutdown now")
