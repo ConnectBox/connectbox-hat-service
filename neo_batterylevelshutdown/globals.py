296d7719df26db4765ecba2f71c8b4c5ad007dbe
Jim Adams <DorJamJr@users.noreply.github.com>
2021-09-16 15:09:13 -0600

    Use dictionary and json to handle branding specifics

--- neo_batterylevelshutdown/globals.py
+++ neo_batterylevelshutdown/globals.py
@@ -2,6 +2,14 @@
 #
 # Here we have a single place to create and make available globals which
 #  will be used in multiple modules
+
+# Example to create and write the dictionary (next four lines)
+#details  = {'Brand':"ConnectBox", 'Image':"connectbox_image.png", 'Font':26,'pos_x': 7,'pos_y': 0}
+#with open('/usr/local/connectbox/brand.txt', 'w') as f:
+#    f.write(json.dumps(details))
+#    f.close()
+
+
 import json
 
 global device_type
@@ -15,35 +23,34 @@ global splash_x
 global splash_y
 global splash_font
 
-brand = ['ConnectBox', "connectbox_image.png", 26, 7, 0]
 
-#find and set device_type global
-device_type = "NEO"
-with open("/proc/cpuinfo", encoding = 'utf8') as f:
-  filx = f.read()
-  if ("Raspberry" in filx):
-    if ("Compute Module" in filx):
-       device_type = "CM"
-    else:           #all other Raspberry Pi version other than compute modules
-       device_type = "PI"
-  f.close()
+def init():
+  # Using a dictionary and json to store Branding stuff
+  # Read the dictionary
+  with open('/usr/local/connectbox/brand.txt') as f:
+    data = f.read()
+  js = json.loads(data)
 
-with open('/etc/hostname', "rw", encoding='utf-8') as f:
-  brand_name = f.read()
-  brand_name = brand_name.rstrip()
-  f.close()
-with open('/usr/local/connectbox/brand.txt', "rw") as f:
-  brand = json.load(f)
-  brand[0] = brand_name
-  logo_image = brand[1]
-  json.dump(brand,f)
-  f.close()
+  brand_name = js["Brand"]
+  logo_image = js["Image"]
+  splash_font = js["Font"]
+  splash_x = js["pos_x"]
+  splash_y = js["pos_y"]
 
+  #find and set device_type global
+  device_type = "NEO"
+  with open("/proc/cpuinfo", encoding = 'utf8') as f:
+    filx = f.read()
+    if ("Raspberry" in filx):
+      if ("Compute Module" in filx):
+        device_type = "CM"
+      else:           #all other Raspberry Pi version other than compute modules
+        device_type = "PI"
+    f.close()
+
+ 
 # set font sizes to work with current PIL calculations for ttf
   font30 = 26
   font20 = 19
   font14 = 13
   font10 = 11
-  splash_font = brand[2]
-  splash_x = brand[3]
-  splash_y = brand[4]
